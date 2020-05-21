ab0c1f11cf6f07f0f3d280fffbf442e402e2ee6c
AshtonRagan <arkjr1@gmail.com>
2020-05-21 06:32:01 -0500

    met mvp

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,36 +1,94 @@
 # TO-DO: complete the helper function below to merge 2 sorted arrays
 def merge(arrA, arrB):
-    elements = len(arrA) + len(arrB)
-    merged_arr = [0] * elements
+    # elements = len(arrA) + len(arrB)
+    merged_arr = []
 
     # Your code here
+    a = 0
+    b = 0
 
+    while a < len(arrA) and b < len(arrB):
+        if arrA[a] < arrB[b]:
+            merged_arr.append(arrA[a])
+            a += 1
+        else:
+            merged_arr.append(arrB[b])
+            b += 1
+
+    while a < len(arrA):
+        merged_arr.append(arrA[a])
+        a += 1
+    while b < len(arrB):
+        merged_arr.append(arrB[b])
+        b += 1
     return merged_arr
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort(arr):
     # Your code here
-
+    if len(arr) > 1:
+        left = merge_sort(arr[:len(arr) // 2])
+        right = merge_sort(arr[len(arr) // 2:])
+        arr = merge(left, right)
     return arr
 
 
 # implement an in-place merge sort algorithm
 def merge_in_place(arr, start, mid, end):
     # Your code here
-
+    # start2 is the first element in the right
+    # half of the list
+    start2 = mid + 1
+    # If the two halves we're merging are already
+    # sorted, no need to do anything
+    if arr[mid] <= arr[start2]:
+        return
+    # Two pointers to maintain start
+    # of both arrays to merge
+    while start <= mid and start2 <= end:
+        # If element 1 is in right place
+        if arr[start] <= arr[start2]:
+            start += 1
+        else:
+            value = arr[start2]
+            index = start2
+            # Shift all the elements between element 1
+            # element 2, right by 1.
+            while index != start:
+                arr[index] = arr[index - 1]
+                index -= 1
+            arr[start] = value
+            # Update all the pointers
+            start += 1
+            mid += 1
+            start2 += 1
     return arr
 
 
 def merge_sort_in_place(arr, l, r):
-    # Your code here
-
-    return arr
+    if l < r:
+        # Same as (l + r) / 2, but avoids overflow
+        # for large l and r
+        m = l + (r - l) // 2
+        # Sort first and second halves
+        merge_sort_in_place(arr, l, m)
+        merge_sort_in_place(arr, m + 1, r)
+        merge_in_place(arr, l, m, r)
+        return arr
 
 
 # STRETCH: implement the Timsort function below
 # hint: check out https://github.com/python/cpython/blob/master/Objects/listsort.txt
-def timsort(arr):
-    # Your code here
+# def timsort(arr):
+#     # Your code here
 
-    return arr
+#     return arr
+
+
+# a = [5, 2, 13, 4, 59, 18, 3, 12, 7, 14, 9, 15]
+# print(merge_sort(a))
+
+# # b = [2, 3, 4, 5, 7, 9, ]
+# # c = [12, 13, 14, 15, 18, 59]
+# # print(merge(b, c))
